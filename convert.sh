69a31aa384c252f4afd1b4a4f905699e8122673b
tanmayanand44 <anand44tanmay@gmail.com>
2019-06-06 01:29:53 +0530

    new script

--- convert.sh
+++ convert.sh
@@ -1,10 +1,24 @@
-folder=$1
-ls $folder > temp
-mkdir -p $2
-for i in $(ls $folder)
-do
-echo "$folder/$i"
-filename=`echo $i | cut -d'.' -f1`
-cupsfilter -i application/vnd.cups-pdf  -m application/vnd.cups-raster "$folder/$i" > "$2/$filename.pwg"
-#echo "$2/$filename.pwg"
-done
+mkdir $2/CityMap
+./pdftoraster 1 1 1 1 "print-color-mode=$1-1" $2/CityMap.pdf > $2/CityMap/$1-1.pwg
+./pdftoraster 1 1 1 1 "print-color-mode=$1-8" $2/CityMap.pdf > $2/CityMap/$1-8.pwg
+./pdftoraster 1 1 1 1 "print-color-mode=$1-16" $2/CityMap.pdf > $2/CityMap/$1-16.pwg
+mkdir $2/document-a4
+./pdftoraster 1 1 1 1 "print-color-mode=$1-1" $2/document-a4.pdf > $2/document-a4/$1-1.pwg
+./pdftoraster 1 1 1 1 "print-color-mode=$1-8" $2/document-a4.pdf > $2/document-a4/$1-8.pwg
+./pdftoraster 1 1 1 1 "print-color-mode=$1-16" $2/document-a4.pdf > $2/document-a4/$1-16.pwg
+mkdir $2/document-letter
+./pdftoraster 1 1 1 1 "print-color-mode=$1-1" $2/document-letter.pdf > $2/document-letter/$1-1.pwg
+./pdftoraster 1 1 1 1 "print-color-mode=$1-8" $2/document-letter.pdf > $2/document-letter/$1-8.pwg
+./pdftoraster 1 1 1 1 "print-color-mode=$1-16" $2/document-letter.pdf > $2/document-letter/$1-16.pwg
+mkdir $2/launch_leaflet
+./pdftoraster 1 1 1 1 "print-color-mode=$1-1" $2/launch_leaflet.pdf > $2/launch-leaflet/$1-1.pwg
+./pdftoraster 1 1 1 1 "print-color-mode=$1-8" $2/launch_leaflet.pdf > $2/launch-leaflet/$1-8.pwg
+./pdftoraster 1 1 1 1 "print-color-mode=$1-16" $2/launch_leaflet.pdf > $2/launch-leaflet/$1-16.pwg
+mkdir $2/onepage-a4
+./pdftoraster 1 1 1 1 "print-color-mode=$1-1" $2/onepage-a4.pdf > $2/onepage-a4/$1-1.pwg
+./pdftoraster 1 1 1 1 "print-color-mode=$1-8" $2/onepage-a4.pdf > $2/onepage-a4/$1-8.pwg
+./pdftoraster 1 1 1 1 "print-color-mode=$1-16" $2/onepage-a4.pdf > $2/onepage-a4/$1-16.pwg
+mkdir $2/onepage-letter
+./pdftoraster 1 1 1 1 "print-color-mode=$1-1" $2/onepage-letter.pdf > $2/onepage-letter/$1-1.pwg
+./pdftoraster 1 1 1 1 "print-color-mode=$1-8" $2/onepage-letter.pdf > $2/onepage-letter/$1-8.pwg
+./pdftoraster 1 1 1 1 "print-color-mode=$1-16" $2/onepage-letter.pdf > $2/onepage-letter/$1-16.pwg
